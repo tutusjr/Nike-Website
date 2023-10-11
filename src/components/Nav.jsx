b6ad6c96e3750598b6f0feec3454e3a08df85561
M.Mustafa Tutus <tutusfullstack@gmail.com>
2023-10-11 21:54:20 +0300

    head progress

---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+import {headerLogo} from '../assets/images'
+import { navLinks } from '../constants'
+import { hamburger } from '../assets/icons'
+
+const Nav = () => {
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className='flex justify-between items-center max-container'>
+            <a href="/">
+                <img src={headerLogo} alt="Logo" width={130} height={29} />
+            </a>
+            <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                {navLinks.map((link) => (
+                    <li key={link.id} className='cursor-pointer'>
+                        <a 
+                        className='font-montserrat text-lg leading-normal text-slate-gray'
+                        href={link.href}>{link.label}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className='hidden max-lg:block'>
+                <img src={hamburger} alt="Hamburger" width={25} height={25} />
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Nav
---
+++ src/components/ShoeCard.jsx
@@ -0,0 +1,8 @@
+
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
+  return (
+    <div className="border-2 rounded-xl "></div>
+  )
+}
+
+export default ShoeCard
