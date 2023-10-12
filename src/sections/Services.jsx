9b85875a7f6156ad103b19ca9c0c297fdb226f8f
M.Mustafa Tutus <tutusfullstack@gmail.com>
2023-10-12 21:58:06 +0300

    services progress

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,16 @@
+import { services } from "../constants"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <div key={service.id}>
+          <img src={service.imgURL} alt={service.label} width={100} height={100}/>
+          <p className="text-slate-gray text-sm">{service.label}</p>
+          <p className="text-slate-gray text-sm">{service.subtext}</p>
+        </div>
+      ))}
+    </section>
   )
 }
 
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,8 +1,33 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
 
 const SuperQuality = () => {
   return (
-    <div>SuperQuality</div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="text-4xl font-palanquin capitalize font-bold lg:max-w-lg">
+          We Provide You <span className="text-coral-red"> Super</span> <span className="text-coral-red">
+            Quality
+          </span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, fugiat nesciunt. Repellat ut ad fuga iusto consequatur illum fugit est aliquid maxime officiis. Alias, natus.
+        </p>
+        <p className="mt-8 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, quaerat.
+        </p>
+        <div className="mt-11 lg:max-w-lg">
+        <Button label="View details"/>
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img className="object-contain" src={shoe8} alt="shoe8" width={570} height={522}/>
+        </div>
+    </section>
+  );
+};
 
-export default SuperQuality
+export default SuperQuality;
